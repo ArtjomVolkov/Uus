b7e4671d67e4d4568c2c02e4fc9be192bf0070dd
ArtjomVolkov <93324363+ArtjomVolkov@users.noreply.github.com>
2022-11-30 21:03:34 +0200

    Add files via upload

---
+++ CinamonCinema/CinamonCinema/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CinamonCinema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(1, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(792, 636);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(1, -2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(792, 70);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 700);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cinamon";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ CinamonCinema/CinamonCinema/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CinamonCinema
+{
+    public partial class Form1 : Form
+    {
+        List<string> arr = new List<string>()
+        {
+            "odin_doma",
+            "ma4o",
+            "time_for_me",
+            "devnaja_smena",
+            "vegas",
+        };
+        List<string> son = new List<string>()
+        {
+            "son_era",
+            "son_requin",
+            "son_artek",
+            "son_crush",
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            PictureBox pictur1 = new PictureBox()
+            {
+                Size = new Size(800, 100),
+                Location = new Point(1, 1),
+                Image = Image.FromFile("../../Images/cinamon.png"),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+            };
+            panel2.Controls.Add(pictur1);
+            panel1.AutoScroll = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                PictureBox pictureBox = new PictureBox() {
+                    Size = new Size(220, 300),
+                    Location = new Point(i * 250, 11),
+                    Image = Image.FromFile("../../Images/" + arr[i] + ".jpg"),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Tag = arr[i]
+                };
+                pictureBox.Click += (e, s) => {
+                    PictureBox pictureBox1 = (e as PictureBox);
+                    Form5 form3 = new Form5(pictureBox1.Tag.ToString() + "");
+                    form3.Show();
+                };
+                panel1.Controls.Add(pictureBox);
+            }
+            for (int i = 0; i < son.Count; i++)
+            {
+                PictureBox pictureBox3 = new PictureBox()
+                {
+                    Size = new Size(220, 300),
+                    Location = new Point(i * 250, 333),
+                    Image = Image.FromFile("../../Soon_film/" + son[i] + ".jpg"),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Tag = son[i]
+                };
+                pictureBox3.Click += (e, s) =>
+                {
+                    MessageBox.Show("Seda filmi ei saa vaadata, kuna see pole veel meie kinos linastunud.","Varsti...",MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+                };
+                panel1.Controls.Add(pictureBox3);
+            }
+        }
+    }
+}
